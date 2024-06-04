48bebeb4d9f38a6883dde8aeaab1ac444b4e7ed9
nkattin <ngkatti@hotmail.com>
2024-06-04 23:22:26 +0100

    Pytest for error handling

--- backend/tests/test_integration.py
+++ backend/tests/test_integration.py
@@ -13,6 +13,11 @@ def test_upload_file():
         assert "histogram" in response.json()
         assert "statistics" in response.json()
 
+def test_invalid_file_upload():
+    response = client.post("api/upload/", files={"file": ("invalid_file.txt", b"Not a CSV content")})
+    assert response.status_code == 400
+    assert response.json() == {"detail": "Invalid file format. Only CSV files are allowed."}
+
 def test_download_file():
     project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir))
     file_path = os.path.join(project_root, "example_data/input_data.csv")
@@ -23,3 +28,8 @@ def test_download_file():
     assert response.status_code == 200
     response = client.get("api/download/stats.csv")
     assert response.status_code == 200
+
+def test_non_existent_file_download():
+    response = client.get("api/download/non_existent_file.csv")
+    assert response.status_code == 404
+    assert response.json() == {"detail": "File not found"}
